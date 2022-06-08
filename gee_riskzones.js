d7eae671bcb2161bca709faa4948a3943b479b9b
João Paulo Just Peixoto <just1982@gmail.com>
2022-06-08 10:07:33 -0300

    Revert "Update GEE script with changes in article"
    This reverts commit 98c78bc86238c95f35adbad6d6ff02551a280360.

--- gee_riskzones.js
+++ gee_riskzones.js
@@ -14,9 +14,9 @@ var fc_edus  = ee.FeatureCollection('users/just1982/riskzones/porto_edus');
 Map.centerObject(fc_risks, 0);
 
 // Add risk levels layers
-Map.addLayer(fc_risks.filter(ee.Filter.eq('class', 0)).draw({color: 'red',  pointRadius: 1}), {opacity: 0.5}, 'Risk Level 0');
+Map.addLayer(fc_risks.filter(ee.Filter.eq('class', 0)).draw({color: 'green',  pointRadius: 1}), {opacity: 0.5}, 'Risk Level 0');
 Map.addLayer(fc_risks.filter(ee.Filter.eq('class', 1)).draw({color: 'yellow', pointRadius: 1}), {opacity: 0.5}, 'Risk Level 1');
-Map.addLayer(fc_risks.filter(ee.Filter.eq('class', 2)).draw({color: 'green',    pointRadius: 1}), {opacity: 0.5}, 'Risk Level 2');
+Map.addLayer(fc_risks.filter(ee.Filter.eq('class', 2)).draw({color: 'red',    pointRadius: 1}), {opacity: 0.5}, 'Risk Level 2');
 
 // Add EDUs positioning layer
 Map.addLayer(fc_edus, {opacity: 1.0}, 'EDUs');
