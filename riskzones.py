860c58024361602b2fe0c7a7e043b576d2308f23
João Paulo Just Peixoto <just1982@gmail.com>
2023-01-08 21:28:42 +0000

    Update copyright text

--- riskzones.py
+++ riskzones.py
@@ -1,7 +1,7 @@
 # encoding:utf-8
 """
 Risk Zones classification
-Copyright (C) 2022 João Paulo Just Peixoto
+Copyright (C) 2022 - 2023 João Paulo Just Peixoto
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
