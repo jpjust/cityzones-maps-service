26949487b5a2a0db84363d7b1881992ce0d8bce0
João Paulo Just Peixoto <just1982@gmail.com>
2023-01-10 15:37:28 +0000

    osmpois: Enable reading PoIs weight in JSON

--- osmpois.py
+++ osmpois.py
@@ -44,15 +44,17 @@ def extract_pois(file: str, pois_types: dict) -> tuple[list, list]:
         for tag in node.iter('tag'):
             node_data[tag.get('k')] = tag.get('v')
         
-        if 'poi_weight' in node_data.keys():
-            node_data['weight'] = float(node_data['poi_weight'])
         nodes[id] = node_data
 
         # If this node already represents the requested pois_types, just add it to
         # the list of POIs
         for node_key in node_data.keys():
             try:
-                if node_data[node_key] in pois_types[node_key]:
+                if node_data[node_key] in pois_types[node_key].keys():
+                    if 'poi_weight' in node_data.keys():
+                        node_data['weight'] = float(node_data['poi_weight'])
+                    else:
+                        node_data['weight'] = pois_types[node_key][node_data[node_key]]['w']
                     pois.append(node_data)
             except KeyError:
                 pass
@@ -61,7 +63,9 @@ def extract_pois(file: str, pois_types: dict) -> tuple[list, list]:
     for way in root.iter('way'):
         id = int(way.get('id'))
 
-        way_data = {}
+        way_data = {
+            'weight': 1.0
+        }
 
         # Ways contain a set of nodes, so we must gather them
         way_nodes = []
@@ -92,10 +96,6 @@ def extract_pois(file: str, pois_types: dict) -> tuple[list, list]:
         # Get the first available node to copy its coordinates
         # (depending on the boundaries of the exported OSM file, some
         # nodes may be out of the map)
-        if 'poi_weight' in way_data.keys():
-            way_data['weight'] = float(way_data['poi_weight'])
-        else:
-            way_data['weight'] = 1.0
         for node in way_nodes:
             if node in nodes:
                 way_data['lat'] = float(nodes[node]['lat'])
@@ -108,7 +108,11 @@ def extract_pois(file: str, pois_types: dict) -> tuple[list, list]:
         # the list of POIs
         for way_key in way_data.keys():
             try:
-                if way_data[way_key] in pois_types[way_key]:
+                if way_data[way_key] in pois_types[way_key].keys():
+                    if 'poi_weight' in way_data.keys():
+                        way_data['weight'] = float(way_data['poi_weight'])
+                    else:
+                        way_data['weight'] = pois_types[way_key][way_data[way_key]]['w']
                     pois.append(way_data)
             except KeyError:
                 pass
@@ -117,7 +121,9 @@ def extract_pois(file: str, pois_types: dict) -> tuple[list, list]:
     for relation in root.iter('relation'):
         id = int(relation.get('id'))
 
-        relation_data = {}
+        relation_data = {
+            'weight': 1.0
+        }
 
         for tag in relation.iter('tag'):
             relation_data[tag.get('k')] = tag.get('v')
@@ -131,13 +137,10 @@ def extract_pois(file: str, pois_types: dict) -> tuple[list, list]:
         # Get the first available way to copy its coordinates
         # (depending on the boundaries of the exported OSM file, some
         # ways may be out of the map)
-        relation_data['weight'] = 1.0
         for way in relation_ways:
             if way in ways:
                 relation_data['lat'] = float(ways[way]['lat'])
                 relation_data['lon'] = float(ways[way]['lon'])
-                if 'poi_weight' in ways[way].keys():
-                    relation_data['weight'] = float(ways[way]['poi_weight'])
                 break
 
         relations[id] = relation_data
@@ -146,7 +149,11 @@ def extract_pois(file: str, pois_types: dict) -> tuple[list, list]:
         # the list of POIs
         for relation_key in relation_data.keys():
             try:
-                if relation_data[relation_key] in pois_types[relation_key]:
+                if relation_data[relation_key] in pois_types[relation_key].keys():
+                    if 'poi_weight' in relation_data.keys():
+                        relation_data['weight'] = float(relation_data['poi_weight'])
+                    else:
+                        relation_data['weight'] = pois_types[relation_key][relation_data[relation_key]]['w']
                     pois.append(relation_data)
             except KeyError:
                 pass
